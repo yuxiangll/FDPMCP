6fd8b1807ac74600b59b0e1f018fca50c4401c03
“yuxiangll” <yuxiangll@foxmail.com>
2023-07-31 18:20:31 +0800

    Main

--- src/net/minecraft/client/renderer/chunk/VisGraph.java
+++ src/net/minecraft/client/renderer/chunk/VisGraph.java
@@ -5,6 +5,9 @@ import java.util.BitSet;
 import java.util.EnumSet;
 import java.util.Queue;
 import java.util.Set;
+
+import net.ccbluex.liquidbounce.FDPClient;
+import net.ccbluex.liquidbounce.features.module.modules.render.XRay;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IntegerCache;
@@ -20,6 +23,7 @@ public class VisGraph
 
     public void func_178606_a(BlockPos pos)
     {
+
         this.field_178612_d.set(getIndex(pos), true);
         --this.field_178611_f;
     }
